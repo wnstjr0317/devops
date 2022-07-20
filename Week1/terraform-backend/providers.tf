9861db82a4f9538a6a8e465474e3d1c6c26d46dc
comentonjh <comento.njh@gmail.com>
2022-07-20 16:31:40 +0900

    Comento

--- Week1/terraform-backend/providers.tf
+++ Week1/terraform-backend/providers.tf
@@ -1,11 +1,13 @@
 terraform {
-  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.22.0"
+    }
+  }
 }
 
 provider "aws" {
   region = var.aws_region
-}
-
-data "aws_availability_zones" "available" {
-  exclude_names = ["ap-northeast-2a","ap-northeast-2c"]
 }
--- Week1/terraform-backend/terraform-backend.tf
+++ Week1/terraform-backend/terraform-backend.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "test-s3-tf-state" {
 
-  bucket = "<생성할 S3 Bucket명>"
+  bucket = "test-comento-s3-tf-state"
 
   tags = {
-    "Name" = "<구분할 임의의 S3 Bucket Tag명>"
+    "Name" = "test-comento-s3-tf-state"
   }
   
 }
@@ -11,7 +11,7 @@ resource "aws_s3_bucket" "test-s3-tf-state" {
 resource "aws_dynamodb_table" "test-ddb-tf-lock" {
 
   depends_on   = [aws_s3_bucket.test-s3-tf-state]
-  name         = "<생성할 DynamoDB Table명>"
+  name         = "test-ddb-tf-lock"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
@@ -21,7 +21,7 @@ resource "aws_dynamodb_table" "test-ddb-tf-lock" {
   }
 
   tags = {
-    "Name" = "<구분할 임의의 DynamoDB Tag명>"
+    "Name" = "test-ddb-tf-lock"
   }
 
 }
