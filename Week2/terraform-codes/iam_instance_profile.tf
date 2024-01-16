bd1f6b8ef4e11fbe661f6c282dfaef5242349046
박준석 <js_park@JunSeok.local>
2024-01-17 01:41:29 +0900

    주석해제

--- Week2/terraform-codes/iam_instance_profile.tf
+++ Week2/terraform-codes/iam_instance_profile.tf
@@ -1,6 +1,6 @@
-#// IAM 인스턴스 프로파일 지정
-#resource "aws_iam_instance_profile" "test-ec2-instance-profile" {
-#  name = "test-ec2-instance-profile"
-#  path = "/"
-#  role = "test-iam-role-ec2-instance-bastion"
-#}
+// IAM 인스턴스 프로파일 지정
+resource "aws_iam_instance_profile" "test-ec2-instance-profile" {
+ name = "test-ec2-instance-profile"
+ path = "/"
+ role = "test-iam-role-ec2-instance-bastion"
+}
