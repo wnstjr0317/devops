abfbf804e8bafb2b26bc02b6a6a9d58ecc081be1
박준석 <js_park@JunSeok.local>
2024-01-17 01:42:20 +0900

    주석해제

--- Week2/terraform-codes/ec2_instance.tf
+++ Week2/terraform-codes/ec2_instance.tf
@@ -1,42 +1,42 @@
-#resource "aws_instance" "test-ec2-bastion" {
-#
-#  // 이름 및 태그
-#  tags = {
-#      Name = "test-ec2-bastion"
-#  }
-#
-#  // Amazon Machine Image(AMI)
-#  ami = "ami-09af799f87c7601fa"
-#
-#  // 인스턴스 유형
-#  instance_type = "t2.micro"
-#
-#  // 키 페어(로그인)
-#  key_name = "test-keypair"
-#
-#  //서브넷
-#  subnet_id = aws_subnet.test-public-subnet[1].id
-#
-#  // 가용 영역
-#  availability_zone = "ap-northeast-2c"
-#
-#  // 퍼블릭 IP 자동 할당
-#  associate_public_ip_address = "true"
-#
-#  // 방화벽(보안 그룹)
-#  vpc_security_group_ids = [aws_security_group.test-sg-bastion.id]
-#
-#  // 스토리지 구성
-#  root_block_device {
-#
-#      // 볼륨 용량(GB)
-#      volume_size = "30"
-#
-#      // 볼륨 타입
-#      volume_type = "gp3"
-#  }
-#
-#  // IAM 인스턴스 프로필 할당
-#  iam_instance_profile = aws_iam_instance_profile.test-ec2-instance-profile.name
-#
-#}
+resource "aws_instance" "test-ec2-bastion" {
+
+ // 이름 및 태그
+ tags = {
+     Name = "test-ec2-bastion"
+ }
+
+ // Amazon Machine Image(AMI)
+ ami = "ami-09af799f87c7601fa"
+
+ // 인스턴스 유형
+ instance_type = "t2.micro"
+
+ // 키 페어(로그인)
+ key_name = "test-ssh-key"
+
+ //서브넷
+ subnet_id = aws_subnet.test-public-subnet[1].id
+
+ // 가용 영역
+ availability_zone = "ap-northeast-2c"
+
+ // 퍼블릭 IP 자동 할당
+ associate_public_ip_address = "true"
+
+ // 방화벽(보안 그룹)
+ vpc_security_group_ids = [aws_security_group.test-sg-bastion.id]
+
+ // 스토리지 구성
+ root_block_device {
+
+     // 볼륨 용량(GB)
+     volume_size = "30"
+
+     // 볼륨 타입
+     volume_type = "gp3"
+ }
+
+ // IAM 인스턴스 프로필 할당
+ iam_instance_profile = aws_iam_instance_profile.test-ec2-instance-profile.name
+
+}
